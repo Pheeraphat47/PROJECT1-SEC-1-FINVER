3377be2b439ea5ecb27f895d1970516e9e9fdf1b
Pheeraphat47 <dpheeraphat2547@gmail.com>
2024-02-13 21:01:38 +0700

    add Landing Page Section

--- src/main.js
+++ src/main.js
@@ -1,25 +1,4 @@
 import './assets/main.css';
 import { createApp } from 'vue';
-import App from './App.vue';
-import Gender from './pages/Gender.vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import FemaleCard from "./pages/FemaleCard.vue";
-import MaleCard from "./pages/MaleCard.vue";
-import Home from "./pages/Home.vue";
-
-
-const routes = [
-    { path: '/Gender', name: "Gender" , component: Gender },
-    { path: '/', name: "HomePage" , component: Home },
-    { path: '/Female', name: "FemaleCard", component: FemaleCard },
-    { path: '/Male',  name: "MaleCard" ,component: MaleCard },
-];
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
-
-
-
-createApp(App).use(router).mount('#app')
+import App from './App.vue'; 
+createApp(App).mount('#app')
